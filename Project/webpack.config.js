7c0b08c5c5855eca945b05e6d775ce57f25aaa20
Agnes K. Rivers <agneskrivers@gmail.com>
2020-07-24 12:15:11 +0700

    Update Format Prettier

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -4,41 +4,41 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const HTMLWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 module.exports = {
-	entry: './src/index.ts',
-	resolve: {
-		extensions: ['.ts', '.js'],
-	},
-	output: {
-		filename: 'app.js',
-		path: path.resolve(__dirname, 'build'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: ['ts-loader', 'eslint-loader'],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.pug$/,
-				use: 'pug-loader',
-			},
-		],
-	},
-	plugins: [
-		new MiniCSSExtractPlugin({
-			filename: 'css/style.css',
-			chunkFilename: '[hash:4].css',
-		}),
-		new HTMLWebpackPlugin({
-			template: './src/index.pug',
-			filename: 'index.html',
-			minify: true,
-		}),
-		new HTMLWebpackPugPlugin(),
-	],
+    entry: './src/index.ts',
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['ts-loader', 'eslint-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-loader',
+            },
+        ],
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'css/style.css',
+            chunkFilename: '[hash:4].css',
+        }),
+        new HTMLWebpackPlugin({
+            template: './src/index.pug',
+            filename: 'index.html',
+            minify: true,
+        }),
+        new HTMLWebpackPugPlugin(),
+    ],
 };
